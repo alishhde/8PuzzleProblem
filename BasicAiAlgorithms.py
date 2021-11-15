84cc75ce0ba66ef6f432904f53c0fb7640f35cfc
Seyyed Ali Shohadaalhosseini <al.shohadaee@gmail.com>
2021-11-15 19:56:03 +0330

    first commit

---
+++ BasicAiAlgorithms.py
@@ -0,0 +1,19 @@
+
+class FormedInformed():
+    def __init__(self, algorithm):
+        pass
+    
+    def aStarAlgorithm(self):
+        pass
+    
+    def ucsAlgorithm(self):
+        pass
+    
+    def greedyAlgorithm(self, spaceState=None, initialState=None, frontier=None, expanded=None, GraphSearch=True):
+        
+        
+        # If we don't want to expand the states that are expanded
+        if GraphSearch:
+            pass
+        
+    
