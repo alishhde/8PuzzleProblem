fd4a6631c8cd7c4c690efb224ffd16e7ae464b5d
Seyyed Ali Shohadaalhosseini <al.shohadaee@gmail.com>
2021-11-15 20:35:34 +0330

    input and output defined for the 8puzzle V0.1.0

--- 8PuzzleProblem.py
+++ 8PuzzleProblem.py
@@ -1,3 +1,18 @@
 import BasicAiAlgorithms
 
 
+
+
+    """
+        We want to use the greedy algorithm in 8 puzzle problem
+        The Goal is like this as the question says
+        
+        [
+            [1, 2, 3],
+            [8, " ", 4], 
+            [7, 6, 5]
+        ] 
+
+        So the argument we send is a list consist of three list.
+        we have also this for the initial states.
+    """
--- BasicAiAlgorithms.py
+++ BasicAiAlgorithms.py
@@ -11,10 +11,14 @@ class FormedInformed():
     
     def greedyAlgorithm(self, spaceState, initialState, frontier=None, expanded=None, GraphSearch=True):
         """
-        This method is going to calculate the greedy algorithm.
+        This method is going to calculate the greedy algorithm. Each time you call this method,
+        it define one best next node. So you must repeat calling this method to reach the Goal,
+        and you must test the goal in your code after you call this function.
 
         Args:
-            spaceState (List): This argument is a sequence of all the  states that we have.
+            spaceState (a of List two list): This argument is a sequence of all the  states and their costs.
+                        first list must be the nodes and the second list must be the cost.
+                        
             initialState ([List]): This argument is the start state, we start searching with this argument.
             frontier ([List]): This is a list of the nodes or states that we must choose to expand.
             expanded ([List]): This is also a list of the nodes or states that we have expanded. Defaults to None.
@@ -22,8 +26,17 @@ class FormedInformed():
                         argument is False then we also change the states that also we have expanded them.
         """
         
-        # If we don't want to expand the states that are expanded
+        # If we don't want to expand the nodes that are expanded, else Tree search
         if GraphSearch:
+            
+            if initialState not in spaceState:
+                return False    # If initial state is empty
+            else:
+                
+                pass
+        
+        else: # When we are using Tree Search algorithm, and we want to also check the nodes that are expanded.
             pass
         
-    
+    def _cost(self, heuristic="NumberOfNotInPlace"):
+        pass
