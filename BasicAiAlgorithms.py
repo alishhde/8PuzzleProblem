1cc82d06814dbfad91b45d19a6bfd6ee9468a1ea
Seyyed Ali Shohadaalhosseini <al.shohadaee@gmail.com>
2021-11-15 20:06:10 +0330

    Greedy Dcumentation added V0.0.1

--- BasicAiAlgorithms.py
+++ BasicAiAlgorithms.py
@@ -9,8 +9,18 @@ class FormedInformed():
     def ucsAlgorithm(self):
         pass
     
-    def greedyAlgorithm(self, spaceState=None, initialState=None, frontier=None, expanded=None, GraphSearch=True):
-        
+    def greedyAlgorithm(self, spaceState, initialState, frontier=None, expanded=None, GraphSearch=True):
+        """
+        This method is going to calculate the greedy algorithm.
+
+        Args:
+            spaceState (List): This argument is a sequence of all the  states that we have.
+            initialState ([List]): This argument is the start state, we start searching with this argument.
+            frontier ([List]): This is a list of the nodes or states that we must choose to expand.
+            expanded ([List]): This is also a list of the nodes or states that we have expanded. Defaults to None.
+            GraphSearch (bool, optional): This argument define to use the expanded list in our program or not. if this 
+                        argument is False then we also change the states that also we have expanded them.
+        """
         
         # If we don't want to expand the states that are expanded
         if GraphSearch:
